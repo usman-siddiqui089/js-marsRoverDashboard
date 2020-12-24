12127bec42b5dd8b7f3a39e2be8c5db9641a9322
UsmanSiddiqui <usmansiddiqui@A006-00349.local>
2020-12-24 11:54:56 +0500

    Fetch api response and create on click event

--- src/public/client.js
+++ src/public/client.js
@@ -6,8 +6,25 @@ let store = {
 
 // add our markup to the page
 const root = document.getElementById('root')
-var textWrapper = document.getElementById('welcome-text')
-const home_content = document.getElementById('home-content')
+const textWrapper = document.getElementById('welcome-text')
+const homeContent = document.getElementById('home-content')
+
+function roverClick(rover) {
+    const roverId = rover.id
+    switch (roverId) {
+        case 'curiosity':
+            getRoverImages(roverId)
+            break;
+        case 'opportunity':
+            getRoverImages(roverId)
+            break;
+        case 'spirit':
+            getRoverImages(roverId)
+            break;
+        default:
+            return false;
+    }
+}
 
 const updateStore = (store, newState) => {
     store = Object.assign(store, newState)
@@ -23,7 +40,7 @@ const render = async (root, state) => {
 // create content
 const App = (state) => {
     let { rovers, apod } = state
-    home_content.style.display = 'block';
+    homeContent.style.display = 'block';
 }
 
 // listening for load event because page should load before any JS is called
@@ -35,58 +52,32 @@ window.addEventListener('load', () => {
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-// const Greeting = (name) => {
-//     if (name) {
-//         return `
-//             <h1>Welcome, ${name}!</h1>
-//         `
-//     }
-
-//     return `
-//         <h1>Hello!</h1>
-//     `
-// }
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
-
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
-    }
-}
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+// // Example API call
+// const getImageOfTheDay = (state) => {
+//     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+//     fetch(`http://localhost:3000/apod`)
+//         .then(res => res.json())
+//         .then(apod => updateStore(store, { apod }))
 
-    return { apod }
+//     return { apod }
+// }
+
+const getWeatherInfo = () => {
+    fetch(`http://localhost:3000/weather`)
+    .then(res => res.json())
+    .then(data => console.log(data.weather))
+}
+
+getWeatherInfo();
+
+const getRoverImages = (rover) => {
+    fetch(`http://localhost:3000/${rover}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
 
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
