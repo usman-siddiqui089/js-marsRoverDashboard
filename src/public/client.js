ceb966dd9218d9ab3b94dd74ed114cd5378b270c
UsmanSiddiqui <usmansiddiqui@A006-00349.local>
2021-01-03 20:17:58 +0500

    completed retrieval of data and display

--- src/public/client.js
+++ src/public/client.js
@@ -1,85 +1,126 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+let store = Immutable.Map({
+    roverImages: ''
+})
 
 // add our markup to the page
-const root = document.getElementById('root')
 const textWrapper = document.getElementById('welcome-text')
 const homeContent = document.getElementById('home-content')
+const windSpan = document.getElementById('wind-speed')
+const tempSpan = document.getElementById('temperature')
+const curiosityContent = document.getElementById('curiosity-content')
+const opportunityContent = document.getElementById('opportunity-content')
+const spiritContent = document.getElementById('spirit-content')
 
 function roverClick(rover) {
-    const roverId = rover.id
-    switch (roverId) {
-        case 'curiosity':
-            getRoverImages(roverId)
+    const roverID = rover.id
+    getRoverImagesApiData(roverID)
+}
+
+const getRoverImagesApiData = (rover) => {
+    fetch(`http://localhost:3000/${rover}`)
+    .then(res => res.json())
+    .then(data => {
+        const newState = updateStore(data)
+        retrieveImagesAndData(newState)
+    })
+}
+
+function retrieveImagesAndData(state){
+    const latestPhotosArr = Array.from(state.get('roverImages').get('latest_photos'))
+    const imgSrcArr = Array.from(Immutable.Seq(latestPhotosArr).map(rover => rover.toObject().img_src))
+    const roverInfo = latestPhotosArr[0].toObject().rover.toObject()
+    const final_data = {
+        imgSrcArr,
+        roverInfo
+    }
+    setContent(final_data.roverInfo.name, generateHtml(final_data))
+}
+
+function setContent(roverName,htmlData){
+    switch(roverName) {
+        case 'Curiosity':
+            curiosityContent.innerHTML = htmlData
             break;
-        case 'opportunity':
-            getRoverImages(roverId)
+        case 'Opportunity':
+            opportunityContent.innerHTML = htmlData
             break;
-        case 'spirit':
-            getRoverImages(roverId)
+        case 'Spirit':
+            spiritContent.innerHTML = htmlData
             break;
         default:
             return false;
     }
 }
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
+function generateHtml(roverObj){
+    return `                                                
+        <div class="tm-img-gallery-info-container">                                    
+            <h2 class="tm-text-title tm-gallery-title"><span class="tm-bold tm-yellow" id="roverName">${roverObj.roverInfo.name}</span></h2>
+            <p class="tm-text"><span class="tm-yellow roverInfoTitle">Launch Date:</span><span class="tm-white roverInfoData" id="roverLaunchDate">&nbsp;${roverObj.roverInfo.launch_date}</span></p>
+            <p class="tm-text"><span class="tm-yellow roverInfoTitle">Landing Date:</span><span class="tm-white roverInfoData" id="roverLandingDate">&nbsp;${roverObj.roverInfo.landing_date}</span></p>
+            <p class="tm-text"><span class="tm-yellow roverInfoTitle">Status:</span><span class="tm-white roverInfoData" id="roverStatus">&nbsp;${roverObj.roverInfo.status}</span></p>
+        </div>      
+        ${generateImageTiles(roverObj.imgSrcArr)}                                                                  
+    `
 }
 
-const render = async (root, state) => {
-    // root.innerHTML = App(state)
-    return App(state)
+function generateImageTiles(imagesArr){
+    let imageGallery = ''
+    for(let i=0; i<imagesArr.length; i++){
+        imageGallery += `
+            <div class="grid-item">
+                <img src="${imagesArr[i]}" alt="Image" class="img-fluid tm-img">
+            </div>
+        `
+    }
+    return imageGallery
 }
 
+const updateStore = (newState) => {
+    const newStore = store.merge(store,newState)
+    return newStore
+}
 
-// create content
-const App = (state) => {
-    let { rovers, apod } = state
+const render = async () => {
+    return App()
+}
+
+const App = () => {
     homeContent.style.display = 'block';
 }
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
     setTimeout(function(){
-        render(root, store)
+        render(store)
+        getWeatherApiData()
     },3500)
 })
 
-// ------------------------------------------------------  COMPONENTS
-
-
-// ------------------------------------------------------  API CALLS
-
-// // Example API call
-// const getImageOfTheDay = (state) => {
-//     let { apod } = state
-
-//     fetch(`http://localhost:3000/apod`)
-//         .then(res => res.json())
-//         .then(apod => updateStore(store, { apod }))
-
-//     return { apod }
-// }
-
-const getWeatherInfo = () => {
+// Retrieve Weather Api Data and Display
+const getWeatherApiData = () => {
     fetch(`http://localhost:3000/weather`)
     .then(res => res.json())
-    .then(data => console.log(data.weather))
+    .then(data => {
+        const weatherState = updateStore(data)
+        retrieveWeatherInfo(weatherState)
+    })
 }
 
-getWeatherInfo();
-
-const getRoverImages = (rover) => {
-    fetch(`http://localhost:3000/${rover}`)
-    .then(res => res.json())
-    .then(data => console.log(data))
+function retrieveWeatherInfo(state){
+    const weatherObj = state.get('weather')
+    const sol_key = weatherObj.get('sol_keys').get(0)
+    const temperature = weatherObj.get(sol_key).get('AT').get('av')
+    const windSpeed = weatherObj.get(sol_key).get('HWS').get('av')
+    generateWeatherHtml(temperature,windSpeed)
 }
 
+function generateWeatherHtml(temp,ws){
+    windSpan.innerHTML = `<span>${ws} m/s</span>`
+    tempSpan.innerHTML = `<span>${temp}&deg; F</span>`
+}
+
+// Preloader Animations Here
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 anime.timeline({loop: false})
   .add({
